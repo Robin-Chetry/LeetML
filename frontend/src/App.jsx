06f0e8fc188d68640e7a9fa849618620981d2d26
Robin-Chetry <robinrawatchetry@gmail.com>
2026-07-30 18:25:16 +0530

    initial work  of frontend started

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-base-200">
+      <button className="btn btn-primary">
+        DaisyUI Working 🚀
+      </button>
+    </div>
+  );
+}
+
+export default App;
